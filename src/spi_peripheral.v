7a63d014e200cd559abe4448774d1440aafd5e65
Rosnaky <ronakjpatelr22@gmail.com>
2025-04-30 22:58:17 -0400

    more typos 🤪🤪🤪

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -13,7 +13,7 @@ module spi_peripheral (
     output reg [7:0] en_reg_out_15_8,
     output reg [7:0] en_reg_pwm_7_0,
     output reg [7:0] en_reg_pwm_15_8,
-    output reg [7:0] pwm_duty_cycle,
+    output reg [7:0] pwm_duty_cycle
 );
 
 
@@ -25,11 +25,11 @@ module spi_peripheral (
             en_reg_pwm_15_8 <= 8'd0;
             pwm_duty_cycle <= 8'd0;
         end else begin
-            en_reg_out_7_0 <= (address == 7'h00) ? data : 7'h00; 
-            en_reg_out_15_8 <= (address == 7'h01) ? data : 7'h00; 
-            en_reg_pwm_7_0 <= (address == 7'h02) ? data : 7'h00; 
-            en_reg_pwm_15_8 <= (address == 7'h03) ? data : 7'h00; 
-            pwm_duty_cycle <= (address == 7'h04) ? data : 7'h00;
+            en_reg_out_7_0 <= (address == 7'h00) ? data : 8'h00; 
+            en_reg_out_15_8 <= (address == 7'h01) ? data : 8'h00; 
+            en_reg_pwm_7_0 <= (address == 7'h02) ? data : 8'h00; 
+            en_reg_pwm_15_8 <= (address == 7'h03) ? data : 8'h00; 
+            pwm_duty_cycle <= (address == 7'h04) ? data : 8'h00;
         end
     end
 
